83d43750ccc6bd2d1d18de0e7b02cf5d8a299a61
smkvdev <tobuy-krd@mail.ru>
2022-12-28 23:06:25 +0300

    dinamic links menu products.html

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from mainapp.models import Product
+
 
 def index(request):
     title = 'каталог'
@@ -10,9 +12,13 @@ def index(request):
         {'href': 'products_modern', 'name': 'модерн'},
         {'href': 'products_classic', 'name': 'классика'},
     ]
+
+    products = Product.objects.all()[:4]
+
     context = {
         'title': title,
         'links_menu': links_menu,
+        'related_products': products,
     }
     return render(request, 'mainapp/products.html', context)
 
